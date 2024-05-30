26ed5e72f6832833977bc937e8518d170cf70891
Vicky AS <vickyhuang808@gmail.com>
2024-05-30 14:21:38 +0700

    membuat controller dan readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Final Project Go Batch 56
+
+## Struktur Folder
+
+```shell
+final-project-golang
+├───config
+├───controllers
+├───database
+│   └───migrations
+├───repositories
+├───routes
+├───structs
+└───utils
+```
+
+## Endpoint untuk Manajemen Kost
+
+Berikut adalah endpoint untuk menggunakan aplikasi ini
+
+BASE URL (local):`http://localhost:8080`
--- controllers/transactionController.go
+++ controllers/transactionController.go
@@ -1,1 +1,86 @@
 package controllers
+
+import (
+	"net/http"
+	"project-indekost/database"
+	"project-indekost/repositories"
+	"project-indekost/structs"
+	"project-indekost/utils"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetLodgerDueDateTransaction(ctx *gin.Context) {
+	var result gin.H
+	transactions, err := repositories.GetDueDateTransaction(database.DBConnection)
+	if err != nil {
+		result = gin.H{
+			"success": false,
+			"message": err.Error(),
+			"data":    utils.Empty,
+		}
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, result)
+	} else {
+		result = gin.H{
+			"success": true,
+			"message": "Berhasil mengambil seluruh data transaction dengan nama penghuni",
+			"data":    transactions,
+		}
+		ctx.JSON(http.StatusOK, result)
+	}
+}
+
+func InsertDataTransaction(ctx *gin.Context) {
+	var transaction structs.Transaction
+	err := ctx.ShouldBindJSON(&transaction)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"message": err.Error(),
+			"data":    utils.Empty,
+		})
+	}
+	err = repositories.InsertTransactionRentData(database.DBConnection, transaction)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"message": err.Error(),
+			"data":    utils.Empty,
+		})
+	} else {
+		ctx.JSON(http.StatusCreated, gin.H{
+			"success": true,
+			"message": "Berhasil menambahkan data rental",
+			"data":    utils.Empty,
+		})
+	}
+}
+
+func DeleteDataTransaction(ctx *gin.Context) {
+	var transaction structs.Transaction
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"message": err.Error(),
+			"data":    utils.Empty,
+		})
+	}
+
+	transaction.ID = id
+	err = repositories.DeleteTransactionRentData(database.DBConnection, transaction)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"message": err.Error(),
+			"data":    utils.Empty,
+		})
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"message": "Berhasil menghapus data lodger",
+			"data":    utils.Empty,
+		})
+	}
+}
